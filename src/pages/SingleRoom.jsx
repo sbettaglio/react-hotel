a201d69778ed74dcb81eeee18ee940437d531164
Stefan Bettaglio <sbettaglio@gmail.com>
2020-07-14 10:03:37 -0400

    styled hero component

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
+import StyledHero from "../components/StyledHero";
 
 class SingleRoom extends Component {
   constructor(props) {
@@ -41,13 +42,13 @@ class SingleRoom extends Component {
       images,
     } = room;
     return (
-      <Hero hero="roomsHero">
+      <StyledHero img={images[0]}>
         <Banner title={`${name} room`}>
           <Link to="/rooms" className="btn-primary">
             Back to rooms
           </Link>
         </Banner>
-      </Hero>
+      </StyledHero>
     );
   }
 }
