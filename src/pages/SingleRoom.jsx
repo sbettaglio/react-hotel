b118edc3cfe8fd89b2b1e501ca120370fcb391ac
Stefan Bettaglio <sbettaglio@gmail.com>
2020-07-27 09:07:08 -0400

    added images to single room

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -41,14 +41,24 @@ class SingleRoom extends Component {
       pets,
       images,
     } = room;
+    //const [mainImg,...defaultImg] = images
     return (
-      <StyledHero img={images[0]}>
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            Back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={images[0] || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              Back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {images.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+        </section>
+      </>
     );
   }
 }
