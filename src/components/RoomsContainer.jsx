8e7322b7c51e738682a7976260b23bbf31890539
Stefan Bettaglio <sbettaglio@gmail.com>
2020-08-10 09:04:51 -0400

    higher order componenet froom RoomContainer

--- src/components/RoomsContainer.jsx
+++ src/components/RoomsContainer.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
 
-const RoomsContainer = () => {
+function RoomsContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <div>
       Hello from rooms container
-      <RoomsFilter />
-      <RoomsList />
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
     </div>
   );
-};
+}
 
-export default RoomsContainer;
+export default withRoomConsumer(RoomsContainer);
+// const RoomsContainer = () => {
+//   return (
+//     <RoomConsumer>
+//       {(value) => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello from rooms container
+//             <RoomsFilter rooms={rooms} />
+//             <RoomsList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// };
