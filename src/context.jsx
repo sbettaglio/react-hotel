52d67580f9c66cb59f4cfbe4a7aa81a56ab5b826
Stefan Bettaglio <sbettaglio@gmail.com>
2020-08-11 08:34:16 -0400

    fixed checkboxes

--- src/context.jsx
+++ src/context.jsx
@@ -54,7 +54,7 @@ class RoomProvider extends Component {
 
   handleChange = (event) => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
     this.setState(
       {
